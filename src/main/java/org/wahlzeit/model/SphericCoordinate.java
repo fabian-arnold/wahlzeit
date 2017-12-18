17d2fee1e23610449700ef5cde042fa74a2fbc44
Fabian Arnold <fabi.arnold@ci4you.com>
2017-12-18 11:55:35 +0100

    Removed equals and hashcode from SphericCoordinate

--- src/main/java/org/wahlzeit/model/SphericCoordinate.java
+++ src/main/java/org/wahlzeit/model/SphericCoordinate.java
@@ -204,39 +204,6 @@ public class SphericCoordinate extends AbstractCoordinate {
   }
 
   @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (!(o instanceof SphericCoordinate)) {
-      return false;
-    }
-
-    SphericCoordinate that = (SphericCoordinate) o;
-
-    if (Double.compare(that.latitude, latitude) != 0) {
-      return false;
-    }
-    if (Double.compare(that.longitude, longitude) != 0) {
-      return false;
-    }
-    return Double.compare(that.radius, radius) == 0;
-  }
-
-  @Override
-  public int hashCode() {
-    int result;
-    long temp;
-    temp = Double.doubleToLongBits(latitude);
-    result = (int) (temp ^ (temp >>> 32));
-    temp = Double.doubleToLongBits(longitude);
-    result = 31 * result + (int) (temp ^ (temp >>> 32));
-    temp = Double.doubleToLongBits(radius);
-    result = 31 * result + (int) (temp ^ (temp >>> 32));
-    return result;
-  }
-
-  @Override
   public String toString() {
     return "SphericCoordinate{" +
         "latitude=" + latitude +
